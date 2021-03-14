d3c18f9840cba351e38f9c7f70905793941e4d84
Dan Krasilnikov <krasilneg@yandex.ru>
2021-03-14 18:17:50 +1000

    theme installation

--- postinstall.js
+++ postinstall.js
@@ -4,12 +4,15 @@ const path = require('path');
 const copydir = require('copy-dir');
 const { exec } = require('child_process');
 
-copydir(`${__dirname}/view`, `${process.cwd}/view/platform`, { cover: true }, (err) => {
-  if (err) {
-    console.error(err);
-  }
-  const inst = exec(`npm install`, { cwd: `${process.cwd}/view/platform/default/static` });
-  inst.on('close', () => {
-    process.exit(0);
+if (process.cwd() !== __dirname) {
+  console.log('installing web ui default theme');
+  copydir(path.join(__dirname, 'view'), path.join(process.cwd(), 'view/platform'), { cover: true }, (err) => {
+    if (err) {
+      console.error(err);
+    }
+    const inst = exec(`npm install`, { cwd: `${process.cwd}/view/platform/default/static` });
+    inst.on('close', () => {
+      process.exit(0);
+    });
   });
-})
+}
